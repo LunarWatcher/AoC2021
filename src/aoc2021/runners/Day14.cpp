cda264682aabf271099f8a3aafb18b9716f42535
Olivia <zoe.i2k1@gmail.com>
2021-12-18 01:45:09 +0100

    Day 14

--- src/aoc2021/runners/Day14.cpp
+++ src/aoc2021/runners/Day14.cpp
@@ -1,22 +1,55 @@
 #include "Day14.hpp"
 #include "aoc2021/util/Loader.hpp"
 #include "stc/StringUtil.hpp"
+#include <algorithm>
+#include <math.h>
 
 namespace aoc {
 
 Day14::Day14() {
     auto lines = loadFile("14");
 
-    this->pairs = lines.at(0);
+    std::string rPairs = lines.at(0);
+    for (int i = 0; i < rPairs.size(); ++i) {
+        freq[rPairs.at(i)]++;
+        if (i != rPairs.size() - 1) {
+            pairs[rPairs.substr(i, 2)]++;
+        }
+    }
     for (int i = 2; i < lines.size(); ++i) {
         auto split = stc::string::split(lines.at(i), " -> ");
-        std::cout << split.at(0) << split.at(1);
         mapping[split.at(0)] = split.at(1);
     }
 }
 
+long long Day14::score(long long runs) {
+
+    for (int i = 0; i < runs; ++i) {
+        size_t pos = 0;
+        auto old = pairs;
+        pairs.clear();
+        for (auto& [pair, count] : old) {
+            //std::cout << pair << ": " << count << std::endl;
+
+            if (mapping.contains(pair)) {
+                std::string map = mapping[pair];
+                pairs[pair.at(0) + map] += count;
+                pairs[map + pair.at(1)] += count;
+                freq[map.at(0)] += count;
+            }
+        }
+    }
+
+    return (std::max_element(freq.begin(), freq.end(), [](const auto& p1, const auto& p2) {
+        return p1.second < p2.second;
+    })->second -
+    std::min_element(freq.begin(), freq.end(), [](const auto& p1, const auto& p2) {
+        return p1.second < p2.second;
+    })->second);
+}
+
 StrPair Day14::run() {
-    return {"", ""};
+    return {std::to_string(score(10)), std::to_string(score(30))};
 }
 
 }
--- src/aoc2021/runners/Day14.hpp
+++ src/aoc2021/runners/Day14.hpp
@@ -3,17 +3,20 @@
 #include "aoc2021/runners/Runner.hpp"
 
 #include <map>
+#include <unordered_set>
 
 namespace aoc {
 
 class Day14 : public Runner {
 private:
-    std::string pairs;
+    std::map<std::string, long long> pairs;
     std::map<std::string, std::string> mapping;
+    std::map<char, long long> freq;
 public:
     Day14();
 
     StrPair run() override;
+    long long score(long long runs);
 };
 
 }
